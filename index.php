95e0893cd189da21687a8248cf4fa33ba3e17b48
Andres Burden <andresburden@gmail.com>
2014-11-20 18:40:14 -0500

    PHP Pages

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+        <link rel="stylesheet" type="text/css" href="style.css">
+    </head>
+    <body>
+        <div class="container">
+            <form>
+                <p class="label">Username</p>
+                <input type="text" name="username"> 
+                <p class="label">Password</p>
+                <input type="password" name="password"> 
+            </form>
+        </div>
+        
+        <?php
+        ?>
+    </body>
+</html>
---
+++ search.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Search Record</title>
+        <link rel="stylesheet" type="text/css" href="style.css">
+    </head>
+    <body>
+        <div class="main_container">
+            <h2>Search Records</h2>
+            <a href="#">enter new record</a>
+            <form>
+                <p>Enter Artist Name or Album:</p>
+                <input class="search" type="text" name="username"> 
+                <input class="go" type="submit" value="GO!"> 
+            </form>
+        </div>
+        
+        <?php
+        ?>
+    </body>
+</html>
